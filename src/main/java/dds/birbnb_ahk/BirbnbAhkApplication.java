0c6b6137f1e6df46ea8d915be807c3dcbc17f767
mikeltaberna05.informatica.com <mikeltaberna05.informatica.com>
2025-04-04 09:23:25 -0300

    initial commit

---
+++ src/main/java/dds/birbnb_ahk/BirbnbAhkApplication.java
@@ -0,0 +1,13 @@
+package dds.birbnb_ahk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirbnbAhkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirbnbAhkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dds/birbnb_ahk/BirbnbAhkApplicationTests.java
@@ -0,0 +1,13 @@
+package dds.birbnb_ahk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirbnbAhkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
